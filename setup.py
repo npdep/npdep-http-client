46058a5aa5d5642e667cda0d48d2c136a64f3249
npdep <183926505+npdep@users.noreply.github.com>
2024-11-03 21:02:04 +0100

    license info changed

--- setup.py
+++ setup.py
@@ -11,10 +11,8 @@ setup(
     description="A client to exfiltrate data through HTTP messages",
     classifiers=[
         "Operating System :: OS Independent ",
-        "Programming Language :: Python :: 3 ",
-        "License :: OSI Approved :: MIT License "
+        "Programming Language :: Python :: 3 "
     ],
-    license="MIT",
     long_description=desc,
     long_description_content_type="text/markdown",
     package_dir={"": "src"},
